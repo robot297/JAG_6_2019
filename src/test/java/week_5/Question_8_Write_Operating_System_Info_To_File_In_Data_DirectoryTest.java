41dcb7002559e7d5ea206e391bd14da38887d33a
clara <clara@clara.com>
2017-09-04 17:42:51 -0500

    Q9 make test more flexible

--- src/test/java/week_5/Question_8_Write_Operating_System_Info_To_File_In_Data_DirectoryTest.java
+++ src/test/java/week_5/Question_8_Write_Operating_System_Info_To_File_In_Data_DirectoryTest.java
@@ -11,27 +11,37 @@ import static org.junit.Assert.*;
  */
 public class Question_8_Write_Operating_System_Info_To_File_In_Data_DirectoryTest {
     @Test
-    public void writeOSName() throws Exception {
+    public void writeOSName()  {
         
         Question_8_Write_Operating_System_Info_To_File_In_Data_Directory q8 = new Question_8_Write_Operating_System_Info_To_File_In_Data_Directory()  ;
         
         q8.writeOSName();
         
         // Read file, verify contains correct name
-    
+        
         try {
             
             BufferedReader reader = new BufferedReader(new FileReader(new File("data", "os.txt")));
+            
+            StringBuilder builder = new StringBuilder();
+            
             String line = reader.readLine();
+            while (line != null) {
+                builder.append(line);
+                line = reader.readLine();
+            }
             reader.close();
             
             assertTrue("Write the operating system name to the file os.txt",
-                    line.contains(System.getProperty("os.name")));
-        
+                    builder.toString().contains(System.getProperty("os.name")));
+            
+            
+            
         } catch (FileNotFoundException f) {
-            fail("os.txt file not found. Make sure your code creates a file called " +
-                    "\nos.txt in the data directory of this project. " +
-                    "\nCreate a data directory if it does not exist. ");
+            fail("os.txt File not found. Make sure your code creates a file called " +
+                    "os.txt in the data directory of this project");
+        } catch (IOException ioe) {
+            fail("Error reading os.txt file. " + ioe.getMessage());
         }
         
     }
